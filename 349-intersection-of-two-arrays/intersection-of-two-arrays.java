d56d99153b1fcea031cba7ef96c464e51a5398d5
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-03-10 07:54:07 +0530

    Time: 2 ms (94.84%) | Memory: 43.5 MB (24.92%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -6,15 +6,14 @@ class Solution {
             set1.add(nums1[i]);
         }
         for(int i=0;i<nums2.length;i++){
-            set2.add(nums2[i]);
+            if(set1.contains(nums2[i]))set2.add(nums2[i]);
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int val : set1){
-            if(set2.contains(val))list.add(val);
-        }
-        int []ans = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            ans[i]=list.get(i);
+       
+        int []ans = new int[set2.size()];
+        int i=0;
+        for(int val : set2){
+            ans[i]=val;
+            i++;
         }
         return ans;
     }
