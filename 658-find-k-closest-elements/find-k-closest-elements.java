27faa0bb5fad506f1d70b732e5fe572173e03dca
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-03-22 21:28:56 +0530

    Time: 44 ms (12.16%) | Memory: 45.6 MB (75.51%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    class Pair{
+        int diff,val;
+        public Pair(int diff,int val){
+            this.diff=diff;
+            this.val=val;
+        }
+    }
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        List<Integer> result=new ArrayList<>();
+        PriorityQueue<Pair> pq=new PriorityQueue<>(new Comparator<Pair>(){
+            public int compare(Pair p1,Pair p2){
+                if(p2.diff==p1.diff) return p2.val-p1.val;
+                return p2.diff-p1.diff;
+            }
+        });
+        
+        for(int num:arr){
+            pq.add(new Pair(Math.abs(x-num),num));
+            if(pq.size()>k) pq.poll();
+        }
+        while(pq.isEmpty()==false) result.add(pq.poll().val);
+        Collections.sort(result);
+        return result;
+    }
+}
