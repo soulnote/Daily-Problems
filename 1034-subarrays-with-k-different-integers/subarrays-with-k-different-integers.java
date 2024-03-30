917f6d8c5cb32a78342635a248b83d8962a42976
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-03-31 02:36:38 +0530

    Time: 46 ms (44.09%) | Memory: 46.4 MB (79.69%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public int countOfSubArray(int[] nums, int k){
+        int n = nums.length, left = 0 , right = 0, count = 0;
+        HashMap<Integer, Integer> numFreq = new HashMap<>();
+        for(; right<n; right++){
+           numFreq.compute(nums[right], (key, value) -> (value==null) ? 1 : value+1);
+
+            while(numFreq.size()>k){
+                numFreq.put(nums[left], numFreq.get(nums[left])-1);
+                if(numFreq.get(nums[left]) == 0) numFreq.remove(nums[left]);
+                left++;
+            }
+            count+=(right-left+1);
+        }
+        return count;
+    }
+
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return countOfSubArray(nums,k)-countOfSubArray(nums,k-1);
+    }
+}
