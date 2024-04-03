90af1dea8de73763ac8988cc93342ffaee63fb56
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-03 19:37:22 +0530

    Time: 146 ms (57.37%) | Memory: 41.6 MB (49.08%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,36 +1,42 @@
-class Solution {
+public class Solution {
     public boolean exist(char[][] board, String word) {
-        int m=board.length,n=board[0].length;
-        if(word.length()>m*n) return false;
+        int m = board.length;
+        int n = board[0].length;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (board[i][j] == word.charAt(0)){
-                    if(cellcheck(board, word, i, j, 0)){
-                        return true;
-                    }
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
                 }
             }
         }
+        
         return false;
     }
-    public boolean cellcheck(char [][]board,String word ,int row ,int col,int idx){
-        if(idx==word.length())return true;
-        if(row>=board.length || row<0 || col<0 || col>=board[0].length || board[row][col]!=word.charAt(idx)){
+    
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
-        if(board[row][col]=='1')return false;
-
-        board[row][col] = '1';
-        int[] r = {-1, 1, 0, 0};
-        int[] c = {0, 0, -1, 1};
-
-        for(int i=0;i<4;i++){
-            boolean ans = cellcheck(board, word, row+r[i], col+c[i], idx+1);
-            if(ans == true)
-                return ans;
+        
+        visited[i][j] = true;
+        
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
         }
-        board[row][col] = word.charAt(idx);
+        
+        visited[i][j] = false;
         return false;
     }
 }
