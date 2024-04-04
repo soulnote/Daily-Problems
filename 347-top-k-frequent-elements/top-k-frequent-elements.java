6fcbf31a6a706fddfbe32f1d573157180e3130b5
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-05 04:01:32 +0530

    Time: 17 ms (22.28%) | Memory: 48.3 MB (66.61%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,21 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if( k == nums.length)
-            return nums;
+      
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) map.put(nums[i], 1);
+            else map.put(nums[i], map.get(nums[i]) + 1);
+        }
 
-        int res[] = new int[k];
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int n = map.size();
+        int[][] arr = new int[n][2];
 
-        for(int n: nums)
-            hm.put(n, hm.getOrDefault(n, 0)+1);
+        int idx = 0;
+        for (Integer i: map.keySet()) {
+            arr[idx][0] = i;
+            arr[idx][1] = map.get(i);
+            idx++;
+        }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> hm.get(b) - hm.get(a));
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
 
-        for(int i: hm.keySet())
-            pq.offer(i);
+        int[] res = new int[k];
+        idx = 0;
 
-        for(int i=0; i< k;i++)
-            res[i] = pq.poll();
+        for (int i = n - 1; i > n - 1 - k; i--) {
+            res[idx++] = arr[i][0];
+        }
         return res;
+
+
     }
 }
