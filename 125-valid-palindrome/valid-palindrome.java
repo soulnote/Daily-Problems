a01ffbe591f6b2056b611205bbf8d0f2184a4373
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-05 04:41:22 +0530

    Time: 2 ms (98.95%) | Memory: 42.6 MB (92.73%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean isPalindrome(String str) {
-        String lowerCaseAlphaStr = convertToLowerCaseAlphabetic(str);
-        System.out.print(lowerCaseAlphaStr);
-        if(lowerCaseAlphaStr.length()==0) return true;
-        int left =0,right=lowerCaseAlphaStr.length()-1;
-        while(left<=right){
-            if(lowerCaseAlphaStr.charAt(left)!=lowerCaseAlphaStr.charAt(right))
-                return false;
-            left++;
-            right--;
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
         }
         return true;
-        
-    }
-    public static String convertToLowerCaseAlphabetic(String str) {
-        StringBuilder result = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                result.append(Character.toLowerCase(c));
-            } 
-        }
-        return result.toString();
     }
 }
