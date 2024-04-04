0ce8c88ee2d762e2ca28c20ed65e743f4b1e6d88
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-04 18:44:25 +0530

    Time: 2 ms (75.53%) | Memory: 56 MB (5.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n= nums.length;
-        int[]ans = new int[n];
-
-        //prefix product allocations
-        int p =1;
-        ans[0]=1;
+        int[] postProduct = new int[n], preProduct =new int[n];
+        preProduct[0]=nums[0];
         for(int i=1;i<n;i++){
-            p*=nums[i-1];
-            ans[i]= p;
+            preProduct[i]= preProduct[i-1]*nums[i];
         }
-
-        // suffix product allocations
-        int s = 1;
+        postProduct[n-1]=nums[n-1];
         for(int i=n-2;i>=0;i--){
-            s*=nums[i+1];
-            ans[i]= ans[i]*s;
+            postProduct[i]=postProduct[i+1]*nums[i];
+        }
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            if(i==0)ans[0]=postProduct[1];
+            else if(i==n-1)ans[n-1]= preProduct[n-2];
+            else ans[i] = preProduct[i-1]*postProduct[i+1];
         }
         return ans;
     }
