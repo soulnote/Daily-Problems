5c8859a4e46a368a5173eed567d224da3ee5aad5
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-05 15:43:23 +0530

    Time: 3 ms (61.07%) | Memory: 41.9 MB (84.95%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -2,24 +2,13 @@ import java.util.Stack;
 
 class Solution {
     public String makeGood(String s) {
-        if (s.length() <= 1) return s;
-
         Stack<Character> st = new Stack<>();
-        st.push(s.charAt(0));
-
-        for (int i = 1; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!st.isEmpty()) {
-                char sch = st.peek();
-                if (Character.isAlphabetic(sch) && Character.isAlphabetic(ch) &&
-                    Character.toLowerCase(sch) == Character.toLowerCase(ch) &&
-                    sch != ch) {
-                    st.pop();
-                } else {
-                    st.push(ch);
-                }
+        
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && Math.abs(c - st.peek()) == 32) {
+                st.pop();
             } else {
-                st.push(ch);
+                st.push(c);
             }
         }
 
