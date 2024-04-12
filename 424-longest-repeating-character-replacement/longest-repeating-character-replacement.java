d43b106070d23575d0fadd98bc6d2a459b8ab10f
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-12 23:13:16 +0530

    Time: 5 ms (98.65%) | Memory: 42.3 MB (73.37%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int [] fqArr = new int[26];
-        int left=0,right=0;
-        int longest = 0,maxFq=0;
-        while(right<s.length()){
+        int n = s.length();
+        int[]fqArr = new int[26];
+        int maxfq = 0;
+        int ans = 0;
+        int left =0,right=0;
+        while(right<n){
             int ch = s.charAt(right)-'A';
             fqArr[ch]++;
-            int len = right-left+1;
-            maxFq = Math.max(maxFq,fqArr[ch]);
-            while(left<right && right-left+1-maxFq>k){
+            maxfq = Math.max(maxfq, fqArr[ch]);
+            while(left<right && right-left+1-maxfq>k){
                 fqArr[s.charAt(left)-'A']--;
                 left++;
-                maxFq = 0;
+                maxfq = 0;
                 for(int i=0;i<26;i++){
-                    maxFq = Math.max(maxFq,fqArr[i]);
+                    maxfq = Math.max(maxfq, fqArr[i]);
                 }
             }
-            longest = Math.max(right-left+1,longest);
+            ans = Math.max(ans, right-left+1);
             right++;
-        }
-        return longest;
+        } 
+        return ans;
     }
 }
