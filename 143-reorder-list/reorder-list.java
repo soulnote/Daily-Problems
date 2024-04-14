aa946e38021ed5b6b2d223607b43b8405b876172
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-14 22:20:46 +0530

    Time: 2 ms (90.17%) | Memory: 48.1 MB (67.06%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,37 +10,34 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head==null || head.next==null || head.next.next==null) return;
-        ListNode fast = head, slow = head;
-        while(fast!=null && fast.next!= null){
-            fast = fast.next.next;
-            slow = slow.next;
+        if(head==null || head.next==null || head.next.next ==null)return ;
+        ListNode slow =head,fast=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow= slow.next;
         }
-        fast= slow.next;
+        fast = slow.next;
         slow.next = null;
-        fast = reverse(fast);
-        slow = head;
-        
-        while(fast!=null){
-            ListNode node1 = slow.next;
-            ListNode node2 = fast.next;
-
-            slow.next = fast;
-            fast.next = node1;
-            slow = node1;
-            fast = node2;
+        ListNode head2 = reverse(fast);
+        ListNode headnew = head;
+        while(head!=null && head2!=null){
+            ListNode nextofhead = head.next;
+            ListNode nextofhead2 = head2.next;
+            head.next= head2;
+            head2.next = nextofhead;
+            head = nextofhead;
+            head2= nextofhead2;
         }
-       
-
+        head = headnew;
     }
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
+    public ListNode reverse(ListNode head){
+        ListNode pre = null;
+        while(head!=null){
             ListNode next = head.next;
-            head.next = prev;
-            prev = head;
+            head.next = pre;
+            pre = head;
             head = next;
         }
-        return prev;
+        return pre;
     }
 }
