a1a87fc4cce75caa3c168fb4e2399c59ef5d1631
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-19 13:57:54 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (48.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,24 +1,37 @@
-public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> merged = new ArrayList<>();
-        int i = 0, j = 0;
-        
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                merged.add(nums1[i++]);
-            } else {
-                merged.add(nums2[j++]);
-            }
+class Solution {
+
+    public double findMedianSortedArrays(int[] a, int[] b) {
+
+        int n1 = a.length, n2 = b.length;
+
+        if(n1>n2)
+        return findMedianSortedArrays(b,a);
+
+        int n = n1+n2;
+        int left = (n1+n2+1)/2;
+
+        int low =0, high = n1;
+
+        while(low<=high){
+
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1>0)?a[mid1-1]:Integer.MIN_VALUE;
+            int l2 = (mid2>0)?b[mid2-1]:Integer.MIN_VALUE;
+            int r1 = (mid1<n1)?a[mid1]:Integer.MAX_VALUE;
+            int r2 = (mid2<n2)?b[mid2]:Integer.MAX_VALUE;
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2==1)
+                return Math.max(l1,l2);
+                else
+                return ((double)(Math.max(l1,l2) + Math.min(r1,r2)))/2.0;
+            }else if(l1>r2)
+                high = mid1-1;
+            else low = mid1+1;
         }
-        
-        while (i < nums1.length) merged.add(nums1[i++]);
-        while (j < nums2.length) merged.add(nums2[j++]);
-        
-        int mid = merged.size() / 2;
-        if (merged.size() % 2 == 0) {
-            return (merged.get(mid-1) + merged.get(mid)) / 2.0;
-        } else {
-            return merged.get(mid);
-        }
+
+        return 0;
     }
 }
