a355dac37af103e9e5ddd9d61b945595802ce63d
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-21 00:14:33 +0530

    Time: 157 ms (34.14%) | Memory: 41.4 MB (69.61%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,36 @@
-public class Solution {
+class Solution {
+    boolean isPresent = false;
+    
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
+        int m = board.length, n = board[0].length;
+        int[][] visited = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+                if (word.charAt(0) == board[i][j]) {
+                    solve(i, j, 0, board, visited, m, n, word);
+                    if (isPresent) return true;
                 }
             }
         }
-        
-        return false;
+        return false;  
     }
     
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
+    public void solve(int i, int j, int idx, char[][] board, int[][] visited, int m, int n, String word) {
+        if (i < 0 || i >= m || j < 0 || j >= n || idx == word.length()) return;
+        if (visited[i][j] == 1) return;
+        
+        if (idx == word.length() - 1 && word.charAt(idx) == board[i][j]) {
+            isPresent = true;
+            return;
         }
         
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
-            return false;
-        }
+        if (word.charAt(idx) != board[i][j]) return;
         
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+        visited[i][j] = 1;
+        solve(i + 1, j, idx + 1, board, visited, m, n, word);
+        solve(i - 1, j, idx + 1, board, visited, m, n, word);
+        solve(i, j + 1, idx + 1, board, visited, m, n, word);
+        solve(i, j - 1, idx + 1, board, visited, m, n, word);
+        visited[i][j] = 0;
     }
 }
