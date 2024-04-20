4c7e369f260e1a359a89ce6ea1ea3af9bff45eab
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-20 14:19:47 +0530

    Time: 2 ms (83.84%) | Memory: 44.1 MB (92.41%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,21 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
+     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(candidates);
-        solve(0,candidates,list,target,0);
-        return ans;
+         List<Integer> curr = new ArrayList<>();
+        solve(0,candidates,curr,target);
+        return res;
     }
-    public void solve(int idx, int[]arr, List<Integer> list, int target,int sum){
-        if(sum>target || idx>=arr.length)return;
-        if(target==sum){
-            ans.add(new ArrayList<>(list));
-            return;
+    void solve(int idx,int[]nums,List<Integer> curr,int target){
+        if(idx>=nums.length || target<0)
+            return ;
+        if(target==0){
+            res.add(new ArrayList<>(curr));
+            return ;
         }
         
-        list.add(arr[idx]);
-        solve(idx,arr,list,target,sum+arr[idx]);
-        list.remove(list.size()-1);
-    
-        solve(idx+1,arr,list,target,sum);
+        curr.add(nums[idx]);
+        solve(idx,nums,curr,target-nums[idx]);
+        curr.remove(curr.size()-1);
+        solve(idx+1,nums,curr,target);
     }
 }
