a85db096a65a1630becc708d882ef0ab008af900
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-22 00:04:19 +0530

    Time: 11 ms (34.30%) | Memory: 45.7 MB (29.76%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -0,0 +1,78 @@
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length,n=heights[0].length;
+        int[][] pacific = new int[m][n];
+        int[][] atlantic = new int[m][n];
+        Queue<int[]> q = new LinkedList<>(); 
+        for(int i=0;i<m;i++){
+            pacific[i][0] = 1;
+            q.add(new int[]{i,0});
+        }
+        for(int j=0;j<n;j++){
+            pacific[0][j] = 1;
+            q.add(new int[]{0,j});
+        }
+        bfs(q,'P',heights,pacific,atlantic);
+
+        q.clear();
+
+        for(int j=0;j<n;j++){
+            atlantic[m-1][j] = 2;
+            q.add(new int[]{m-1,j});
+        }
+        for(int i=0;i<m;i++){
+            atlantic[i][n-1] = 2;
+            q.add(new int[]{i,n-1});
+        }
+       bfs(q,'A',heights,pacific,atlantic);
+
+    //    for(int i = 0 ; i< m ; i++){
+    //     for(int j = 0 ; j < n; j ++){
+    //         System.out.print(pacific[i][j] + " ");
+    //     }
+    //     System.out.println();
+    //    }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pacific[i][j]==1 && atlantic[i][j]==2){
+                    ans.add(new ArrayList<>(Arrays.asList(i,j)));
+                }
+            }
+        }
+        return ans;
+    }
+    int[]dR = new int[]{1,-1,0,0};
+    int[]dC = new int[]{0,0,1,-1};
+    public void bfs(Queue<int[]>q,char ocean,int[][]grid,int[][]pacific,int[][]atlantic){
+        while(!q.isEmpty()){
+            // System.out.println("Jai Shree Ram");
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[] present = q.poll();
+                int row = present[0];
+                int col = present[1];
+
+
+                for(int k=0;k<4;k++){
+                    int nRow = row+dR[k];
+                    int nCol = col+dC[k];
+
+                    if(nRow<grid.length && nRow>=0 && nCol>=0 && nCol<grid[0].length && grid[nRow][nCol]>=grid[row][col]){
+                        // System.out.println(nRow + " " + nCol );
+                        if (ocean == 'P' && pacific[nRow][nCol]==0) {
+                            // System.out.println(nRow + " " + nCol );
+                            pacific[nRow][nCol] = 1;
+                            q.offer(new int[]{nRow, nCol});
+                        }
+                        if (ocean == 'A' && atlantic[nRow][nCol]==0) {
+                            atlantic[nRow][nCol] = 2;
+                            q.offer(new int[]{nRow, nCol});
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
