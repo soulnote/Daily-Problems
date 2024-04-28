9792df3168d3f6c8c32854c2d79a32c46373a771
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-28 18:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (55.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         int n= nums.length;
-        if(n==1)return nums[0];
+        // if(n==1)return nums[0];
         int[]dp = new int[n+1];
         Arrays.fill(dp,-1);
         return helper(nums,n-1, dp);
