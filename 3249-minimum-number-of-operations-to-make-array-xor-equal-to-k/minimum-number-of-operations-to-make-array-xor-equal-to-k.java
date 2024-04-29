62a2423bb155ca0be4202f47f838aeebabbcf452
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-04-30 01:31:58 +0530

    Time: 1 ms (100.00%) | Memory: 56.4 MB (55.79%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        
+        int flipCount = 0;
+        int xorSum = 0;
+        for(int data: nums){
+            xorSum ^= data;
+        }
+
+        for(int i=0; i<32; i++){
+            int mask = 1<<i;
+            int oneBitCount = 0;
+            
+            if((xorSum & mask) != (k & mask)){
+                flipCount++;
+            }
+        }
+
+        return flipCount;
+    }
+}
