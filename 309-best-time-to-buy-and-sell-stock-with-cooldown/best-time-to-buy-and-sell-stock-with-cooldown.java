33a6358878d684dd0b219012524ae3d5dd99ff95
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-05-04 19:42:01 +0530

    Time: 1 ms (81.27%) | Memory: 41.3 MB (91.20%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,25 +2,26 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         if(n==1)return 0;
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        return profit(prices, 0, true, map);
+        int[][]dp = new int[n][2];
+        for(int[]num : dp){
+            Arrays.fill(num, -1);
+        }
+        return profit(prices, 0, 1, dp);
     }
-    public int profit(int[]prices, int idx, boolean buying, HashMap<String, Integer> map){
+    public int profit(int[]prices, int idx, int buying, int[][]dp){
         if(idx>= prices.length)return 0;
-        String key = idx + "-" + buying;
-        if(map.containsKey(key)) return map.get(key);
+
+        if(dp[idx][buying]!=-1) return dp[idx][buying];
         //not paticipating on that day
-        int notParticipated =  profit(prices, idx+1, buying, map);
+        int notParticipated =  profit(prices, idx+1, buying, dp);
         int participated = Integer.MIN_VALUE;
-        if(buying){
-            participated = profit(prices, idx+1, !buying, map) - prices[idx];
+        if(buying==1){
+            participated = profit(prices, idx+1, Math.abs(buying-1), dp) - prices[idx];
         }
         else{
-            participated = profit(prices, idx+2, !buying, map) + prices[idx];
+            participated = profit(prices, idx+2, Math.abs(buying-1), dp) + prices[idx];
         }
 
-        map.put(key, Math.max(participated, notParticipated));
-        return map.get(key);
+        return dp[idx][buying] = Math.max(participated, notParticipated);
     }
 }
