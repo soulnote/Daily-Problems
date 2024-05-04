72802a200c135d07d7e1820b402de0c0c0a39c2b
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-05-04 14:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (43.43%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,16 +4,16 @@ class Solution {
         for(int[]num : dp){
             Arrays.fill(num,-1);
         }
-        return solve(m-1,n-1, m, n,dp);
+        return solve(m-1,n-1,dp);
         
     }
-    public int solve(int i, int j, int m, int n, int[][]dp){
+    public int solve(int i, int j, int[][]dp){
         //base case
         if(i==0 && j==0)return 1;
-        if(i<0 || i>=m || j<0 || j>=n)return 0;
+        if(i<0 || j<0)return 0;
         // memoization
         if(dp[i][j]!=-1)return dp[i][j];
         //recursive case
-        return dp[i][j]= solve(i-1,j,m,n,dp)+solve(i,j-1, m,n,dp);
+        return dp[i][j]= solve(i-1,j,dp)+solve(i,j-1,dp);
     }
 }
