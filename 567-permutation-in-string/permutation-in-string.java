9b26b79a5fda52a61ae023e7532b5fca2b1d0b9d
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-05-06 23:23:53 +0530

    Time: 4 ms (97.88%) | Memory: 42.2 MB (95.90%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-        
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-        
-        for (char c : s1.toCharArray()) {
-            count1[c - 'a']++;
+        int n1= s1.length();
+        int n2 = s2.length();
+        if(n1>n2) return false;
+        int []fqArr = new int[26];
+        for(int i=0;i<n1;i++){
+            fqArr[s1.charAt(i)-'a']++;
         }
-        
-        for (int i = 0; i < s2.length(); i++) {
-            count2[s2.charAt(i) - 'a']++;
-            if (i >= s1.length()) {
-                // Remove the leftmost character from the window
-                count2[s2.charAt(i - s1.length()) - 'a']--;
-            }
-            if (Arrays.equals(count1, count2)) {
-                return true;
-            }
+        for(int i=0;i<n1;i++){
+            fqArr[s2.charAt(i)-'a']--;
         }
-        
+        if(isPresent(fqArr))return true;
+        int left = 0,right= n1;
+        while(right<n2){
+            fqArr[s2.charAt(left)-'a']++;
+            fqArr[s2.charAt(right)-'a']--;
+            if(isPresent(fqArr)) return true;
+            left++;
+            right++;
+        }
         return false;
+    }
+    public boolean isPresent(int[]arr){
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)return false;
+        }
+        return true;
     }
 }
