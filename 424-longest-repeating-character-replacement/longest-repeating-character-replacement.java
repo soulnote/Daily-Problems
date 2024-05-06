d0025cca45c7b18357d6a5e19978f92590551616
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-05-06 21:06:03 +0530

    Time: 10 ms (65.98%) | Memory: 42.4 MB (61.23%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLength = 0;
-        int maxCount = 0;
-        int start = 0;
-        int[] charCounts = new int[26]; // Assuming only uppercase English letters
-        
-        for (int end = 0; end < s.length(); end++) {
-            int charIndex = s.charAt(end) - 'A';
-            charCounts[charIndex]++;
-            maxCount = Math.max(maxCount, charCounts[charIndex]);
-            
-            int width = end-start+1;
-            if (width - maxCount > k) {//if window have more number of non repeating characters than k 
-                // Shrink the window from the left side
-                charCounts[s.charAt(start) - 'A']--;
+        int start =0;
+        int max = 0;
+        int maxFq =0;
+        int []charArr = new int[26];
+        for(int i=0;i<s.length();i++){
+            int ch = s.charAt(i)-'A';
+            charArr[ch]++;
+            maxFq = Math.max(maxFq, charArr[ch]);
+
+            int nonRepeatings = (i-start+1) - maxFq;
+            if(nonRepeatings>k){
+                // decrease the frequency of the character from the array which we are eleminating
+                charArr[s.charAt(start)-'A']--;
+                //shrink the window size from left
                 start++;
+
             }
-            maxLength = Math.max(maxLength, end - start + 1);
+            max = Math.max(max, i-start+1);
         }
-        
-        return maxLength;
+        return max;
     }
 }
