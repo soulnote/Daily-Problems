df6f709f5db9b5f7f9364927929e73bde515cda6
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-05-27 01:08:15 +0530

    Time: 305 ms (56.27%) | Memory: 71.4 MB (35.13%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,31 +1,47 @@
 class Solution {
-    private static final int MOD = 1000000007;
 
-    private int check_all_records(int cur_ind, int count_a, int count_l, int n, int[][][] temp) {
-        if (cur_ind == n) {
-            return 1;
-        }
-        if (temp[cur_ind][count_a][count_l] != -1) {
-            return temp[cur_ind][count_a][count_l];
-        }
-        int with_a_next = (count_a == 0) ? check_all_records(cur_ind + 1, count_a + 1, 0, n, temp) : 0;
-        int with_l_next = (count_l == 2) ? 0 : check_all_records(cur_ind + 1, count_a, count_l + 1, n, temp);
-        int with_p_next = check_all_records(cur_ind + 1, count_a, 0, n, temp);
-        int total = ((with_a_next + with_l_next) % MOD + with_p_next) % MOD;
-
-        temp[cur_ind][count_a][count_l] = total;
-        return total;
-    }
+    private static final int MOD = 1000000000 + 7;
 
     public int checkRecord(int n) {
-        int[][][] temp = new int[n][2][3];
-        for (int i = 0; i < n; i++) {
+        int[][][] temp = new int[n + 1][2][3];
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 3; k++) {
                     temp[i][j][k] = -1;
                 }
             }
         }
-        return check_all_records(0, 0, 0, n, temp);
+        return numPossibleRecords(n, 1, 2, temp);
+    }
+
+    private int numPossibleRecords(int n, int numAbsencesRemaining, int numLatesRemaining, int[][][] temp) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (temp[n][numAbsencesRemaining][numLatesRemaining] != -1) {
+            return temp[n][numAbsencesRemaining][numLatesRemaining];
+        }
+
+        int total = 0;
+
+        // Pick P
+        total += numPossibleRecords(n - 1, numAbsencesRemaining, 2, temp);
+        total %= MOD;
+
+        if (numAbsencesRemaining > 0) {
+            // Pick A
+            total += numPossibleRecords(n - 1, numAbsencesRemaining - 1, 2, temp);
+            total %= MOD;
+        }
+
+        if (numLatesRemaining > 0) {
+            // Pick L
+            total += numPossibleRecords(n - 1, numAbsencesRemaining, numLatesRemaining - 1, temp);
+            total %= MOD;
+        }
+
+        temp[n][numAbsencesRemaining][numLatesRemaining] = total;
+        return total;
     }
 }
