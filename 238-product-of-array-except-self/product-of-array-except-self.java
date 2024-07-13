b6240c5fc1ff5dcf042502a4b7a6bab5f80447e1
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-07-13 17:07:23 +0530

    Time: 2 ms (86.27%) | Memory: 55.8 MB (33.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n= nums.length;
-        int[] postProduct = new int[n], preProduct =new int[n];
-        preProduct[0]=nums[0];
+        int n  = nums.length;
+        int[]prefixProduct = new int[n];
+        int[]postfixProduct = new int[n];
+
+        prefixProduct[0] = nums[0];
         for(int i=1;i<n;i++){
-            preProduct[i]= preProduct[i-1]*nums[i];
+            prefixProduct[i] = prefixProduct[i-1]*nums[i];
         }
-        postProduct[n-1]=nums[n-1];
+        postfixProduct[n-1] = nums[n-1];
         for(int i=n-2;i>=0;i--){
-            postProduct[i]=postProduct[i+1]*nums[i];
+            postfixProduct[i] = postfixProduct[i+1]*nums[i];
         }
-        int[] ans = new int[n];
+        int[]ans = new int[n];
         for(int i=0;i<n;i++){
-            if(i==0)ans[0]=postProduct[1];
-            else if(i==n-1)ans[n-1]= preProduct[n-2];
-            else ans[i] = preProduct[i-1]*postProduct[i+1];
+            if(i==0){
+                ans[i] = postfixProduct[i+1];
+            }
+            else if(i==n-1){
+                ans[i] = prefixProduct[i-1];
+            }
+            else{
+                ans[i] = prefixProduct[i-1]*postfixProduct[i+1];
+            }
         }
         return ans;
     }
