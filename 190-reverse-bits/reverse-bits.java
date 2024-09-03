7312eb782533f0193e050901cb6ac384c012f9c8
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-09-03 23:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (24.51%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            // Extract the rightmost bit of n and add it to result
+            result <<= 1;
+            result |= (n & 1);
+            // Shift n to the right to process the next bit
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
