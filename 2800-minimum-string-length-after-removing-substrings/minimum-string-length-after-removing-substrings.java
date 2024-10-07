0950fe12251b36935bc70d08737cc6504370cc3a
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-10-07 11:06:01 +0530

    Time: 6 ms (46.21%) | Memory: 43.2 MB (60.85%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,16 +1,15 @@
+import java.util.Stack;
+
 class Solution {
     public int minLength(String s) {
-        // StringBUilder sb = new StringBuilder(s);
-        boolean isPresent = true;
-        while(s.length()>0 && isPresent==true){
-            isPresent = false;
-            for(int i =1;i<s.length();i++){
-                if(s.substring(i-1,i+1).equals("AB")||s.substring(i-1,i+1).equals("CD")){
-                    s = s.substring(0,i-1)+""+s.substring(i+1,s.length());
-                    isPresent = true;
-                }
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && ((stack.peek() == 'A' && ch == 'B') || (stack.peek() == 'C' && ch == 'D'))) {
+                stack.pop(); 
+            } else {
+                stack.push(ch);
             }
         }
-        return s.length();
+        return stack.size();
     }
 }
