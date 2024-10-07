83e1893da1196d9f32d42152b71cb007b5dd4783
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-10-07 13:18:24 +0530

    Time: 18 ms (70.43%) | Memory: 45.2 MB (42.28%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,32 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        int  begenning = 0;
-        int length = 0;
-        for(int current = 0 ; current <n; current++){
-            // handling odd length case
-            int left = current, right = current;
-            while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
-                if(right-left+1>length){
-                    length = right-left+1;
-                    begenning =left;
-                    
-                }
-                left--;
-                right++;
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        return solve(s);
+    }
+
+    public String solve(String s) {
+        String longest = "";
+        for (int i = 0; i < s.length(); i++) {
+            // Find the longest palindrome with center at i (odd length)
+            String oddPalindrome = expandAroundCenter(s, i, i);
+            if (oddPalindrome.length() > longest.length()) {
+                longest = oddPalindrome;
             }
-            left = current;
-            right = current+1;
-            while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
-                if(right-left+1>length){
-                    length = right-left+1;
-                    begenning = left;
-                   
-                }
-                left--;
-                right++;
+
+            // Find the longest palindrome with center between i and i+1 (even length)
+            String evenPalindrome = expandAroundCenter(s, i, i + 1);
+            if (evenPalindrome.length() > longest.length()) {
+                longest = evenPalindrome;
             }
         }
-        return s.substring(begenning, begenning+length);
+        return longest;
+    }
+
+    // Helper function to expand around the center and find the longest palindrome
+    private String expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 }
