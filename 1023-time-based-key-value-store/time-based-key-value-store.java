2350892b935fe71c308ae54057265a70e2f4bf66
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-10-22 20:47:54 +0530

    Time: 134 ms (84.78%) | Memory: 118.4 MB (77.66%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,20 +1,52 @@
 class TimeMap {
-    Map<String, TreeMap<Integer, String>> map;
+    class Store{
+        String val;
+        int time;
+        Store(String value, int time){
+            this.val  = value;
+            this.time = time;
+        }
+    }
+    HashMap<String,List<Store>> map;
     public TimeMap() {
-        map=new HashMap<>();
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        TreeMap<Integer, String> tm=map.getOrDefault(key, new TreeMap<>());
-        tm.put(timestamp, value);
-        map.put(key, tm);
+        // List<Store> list = map.get(key);
+        // list.add(new Store(value, timestamp));
+        // map.put(key, list);
+        if(!map.containsKey(key)){
+            List<Store> newList = new ArrayList<>();
+            map.put(key, newList);
+        }
+        map.get(key).add(new Store(value, timestamp));
+        // map.put(key, map.get(key).add(new Store(value, timestamp)));
     }
     
     public String get(String key, int timestamp) {
-        if(!map.containsKey(key)) return "";
-        TreeMap<Integer, String> tm=map.get(key);
-        Map.Entry<Integer, String> e=tm.floorEntry(timestamp);
-        if(e==null) return "";
-        else return e.getValue();
+        List<Store> list = map.get(key);
+        if(!map.containsKey(key))return "";
+        if(list.size()==0)return "";
+        int left =0, right = list.size()-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(list.get(mid).time == timestamp)return list.get(mid).val;
+            else if(list.get(mid).time > timestamp){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        if(right<0)return "";
+        return list.get(right).val;
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
