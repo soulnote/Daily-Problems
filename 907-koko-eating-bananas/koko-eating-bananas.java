0caefc0173e0d3743dbb9963ac49795601a74c4a
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-10-22 18:32:10 +0530

    Time: 13 ms (71.14%) | Memory: 44.9 MB (62.83%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,26 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int maxEat = 0;
-        for (int num : piles) {
+        for(int num : piles){
             maxEat = Math.max(num, maxEat);
         }
-
-        int left = 1, right = maxEat;
-        int ans = maxEat;
-        while (left <= right) {
+        int left  = 1,right =maxEat;
+        int ans = 0;
+        while(left<=right){
             int hRemaining = h;
-            int minEat = left + (right - left) / 2;
-
-            for (int pile : piles) {
-                hRemaining -= Math.ceil((double) pile / minEat);
+            int idx = 0;
+            int minEat = left+(right-left)/2;
+            while(idx<piles.length){
+                int banana = piles[idx];
+                hRemaining-= (banana/minEat);
+                banana%=minEat;
+                if(banana>0)hRemaining--;
+                idx++;
             }
-
-            if (hRemaining >= 0) {
+            if(hRemaining>=0){
                 ans = minEat;
-                right = minEat - 1;
-            } else {
-                left = minEat + 1;
+                right = minEat-1;
             }
+            else{
+                left = minEat+1;
+            }
+            
         }
         return ans;
     }
