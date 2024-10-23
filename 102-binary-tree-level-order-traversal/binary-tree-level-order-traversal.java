2bcd5013fa81adfd4303f144849dc1ae2cdbb264
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-10-23 21:31:34 +0530

    Time: 1 ms (90.21%) | Memory: 44.8 MB (74.75%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,35 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
-            return ans; 
-        }
-        Queue<TreeNode> q= new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
+        while (!queue.isEmpty()) {
             List<Integer> list = new ArrayList<>();
-            int size = q.size();
+            int size = queue.size();
             for(int i=0;i<size;i++){
-                TreeNode t = q.poll();
-                if(t==null)continue;
-                list.add(t.val);
-                if(t.left!=null)q.offer(t.left);
-                if(t.right!=null)q.offer(t.right);
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null)queue.add(node.left);
+                if(node.right!=null)queue.add(node.right);
             }
             ans.add(list);
         }
