ef36009504c4db0afe4742d797fd3037e5b40327
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-04 22:08:36 +0530

    Time: 15 ms (54.35%) | Memory: 45.5 MB (76.75%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -1,10 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
 public class Codec {
-
+    
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         List<String> res = new ArrayList<>();
@@ -24,22 +35,20 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        List<String> vals = Arrays.asList(data.split(","));
-        Iterator<String> iterator = vals.iterator();
-        return dfsDeserialize(iterator);
+        String[] vals = data.split(",");
+        int[] i = {0};
+        return dfsDeserialize(vals, i);
     }
 
-    private TreeNode dfsDeserialize(Iterator<String> iterator) {
-        if (!iterator.hasNext()) return null;
-
-        String val = iterator.next();
-        if (val.equals("N")) {
+    private TreeNode dfsDeserialize(String[] vals, int[] i) {
+        if (vals[i[0]].equals("N")) {
+            i[0]++;
             return null;
         }
-        
-        TreeNode node = new TreeNode(Integer.parseInt(val));
-        node.left = dfsDeserialize(iterator);
-        node.right = dfsDeserialize(iterator);
+        TreeNode node = new TreeNode(Integer.parseInt(vals[i[0]]));
+        i[0]++;
+        node.left = dfsDeserialize(vals, i);
+        node.right = dfsDeserialize(vals, i);
         return node;
     }
 }
