6ce8a1ba19005b1d3894c79a0d3104e35fcfff58
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-05 02:02:53 +0530

    Time: 3 ms (7.91%) | Memory: 44.6 MB (21.37%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ret = new ArrayList<>();
-        dfs(ret, new LinkedHashSet<>(), nums);
-        return ret;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, new LinkedHashSet<>(), nums);
+        return res;
     }
     
-    private void dfs(List<List<Integer>> ret, Set<Integer> path, int[] nums) {
+    private void dfs(List<List<Integer>> res, Set<Integer> path, int[] nums) {
         if (path.size() == nums.length) {
-            ret.add(new ArrayList<>(path));
+            res.add(new ArrayList<>(path));
             return;
         }
         for (int num : nums) {
-            if (path.add(num)) {
-                dfs(ret, path, nums);
+            if(!path.contains(num)) {
+                path.add(num);
+                dfs(res, path, nums);
                 path.remove(num);
             }
         }
