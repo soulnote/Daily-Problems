0f670e78013d6c86dd9cd9f4d17974d1839f843a
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-06 05:50:56 +0530

    Time: 7 ms (22.86%) | Memory: 43.6 MB (99.37%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        int arr[] = new int[101];
-        for(int i=0;i<nums.length;i++) arr[i] = noofbits(nums[i]);
+        int n = nums.length;
+        boolean swapped;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (issorted(nums)) return true;
-            for (int j = 1; j < nums.length; j++) {
-                if (arr[j] == arr[j - 1] && nums[j] < nums[j - 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j - 1];
-                    nums[j - 1] = temp;
+        for (int pass = 0; pass < n - 1; pass++) {
+            swapped = false;
+            
+            for (int i = 0; i < n - pass - 1; i++) {
+                int bits1 = countBits(nums[i]);
+                int bits2 = countBits(nums[i + 1]);
+                
+                // Swap if they have the same set bits and are out of order
+                if (bits1 == bits2 && nums[i] > nums[i + 1]) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapped = true;
                 }
             }
+            
+            // Early exit if no swaps were made in this pass
+            if (!swapped) break;
         }
-        return false;
+        
+        // Final check to see if the array is sorted
+        return isSorted(nums);
     }
     
-    public int noofbits(int n){
+    // Count number of set bits in a number
+    private int countBits(int n) {
         int count = 0;
         while (n > 0) {
             n &= (n - 1);
@@ -25,10 +37,10 @@ class Solution {
         return count;
     }
     
-    public boolean issorted(int arr[]){
+    // Check if the array is sorted in ascending order
+    private boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1])
-                return false;
+            if (arr[i] > arr[i + 1]) return false;
         }
         return true;
     }
