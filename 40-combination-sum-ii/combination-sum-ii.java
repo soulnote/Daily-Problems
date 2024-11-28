2f4e2053f4c4a5778a7928d1906afbe4e95ae37e
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-28 15:06:54 +0530

    Time: 3 ms (78.98%) | Memory: 43.2 MB (41.01%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> list = new LinkedList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
-        return list;
+        solve(0, candidates, target, new ArrayList<>(),ans);
+        return ans;
     }
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
-   
-        if(remain < 0) return; /** no solution */
-        else if(remain == 0) list.add(new ArrayList<>(tempList));
-        else{
-            for (int i = start; i < cand.length; i++) {
-            if(i > start && cand[i] == cand[i-1]) continue; /** skip duplicates */
-            tempList.add(cand[i]);
-            backtrack(list, tempList, cand, remain - cand[i], i+1);
-            tempList.remove(tempList.size() - 1);
-            }
+    public void solve(int idx, int[]candidates, int target, List<Integer> list, List<List<Integer>> ans){
+       
+        if(target == 0){
+            ans.add(new ArrayList<>(list));
+            return;
         }
+        for(int i=idx;i<candidates.length;i++){
+            if(i>idx && candidates[i]==candidates[i-1])continue;
+            if(candidates[i]>target)break;
+            list.add(candidates[i]);
+            solve(i+1,candidates,target-candidates[i],list,ans);
+            list.remove(list.size()-1);
+        }
+        
     }
 }
