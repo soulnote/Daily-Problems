b8f03f3c8cda4028ed3a6eecf7f31572ca9a137e
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-29 19:55:11 +0530

    Time: 3 ms (30.61%) | Memory: 44.8 MB (24.77%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,35 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int mArea = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int maxArea = 0;
+        int m = grid.length, n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    int area = dfs(i,j,grid);
-                    mArea = Math.max(mArea,area);
+                    maxArea = Math.max(maxArea, bfs(grid, i, j));
                 }
             }
         }
-        return mArea;
+        return maxArea;
     }
-    
-    int[] dRow = new int[] {0,0,1,-1};
-    int[] dCol = new int[] {1,-1,0,0};
-
-    public int dfs(int r, int c, int[][]grid){
-        // base case 
-        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==0) return 0;
-        grid[r][c] = 0;
-        // recursive case
-        int ans = 1;
-        for(int i = 0 ; i < 4; i++){
-            int newRow = r+dRow[i];
-            int newCol = c+dCol[i];
-            ans+=dfs(newRow, newCol, grid);
+    public int bfs(int[][]grid, int i , int j){
+        int[]dr = {1, -1, 0, 0};
+        int[]dc = {0, 0, 1, -1};
+        grid[i][j]=0;
+        int area = 1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+        while(q.size()>0){
+            int[]temp = q.poll();
+            for(int k=0;k<4;k++){
+                int nr = temp[0]+dr[k];
+                int nc = temp[1]+dc[k];
+                if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
+                    area++;
+                    q.add(new int[]{nr, nc});
+                    grid[nr][nc] = 0;
+                }
+            }
         }
-        return ans;
-     }
+        return area;
+    }
 }
