e5e875708932f1fb158755851420f0d63400f504
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-11-29 19:57:15 +0530

    Time: 2 ms (70.43%) | Memory: 44.7 MB (41.36%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,35 +1,31 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
-        int m = grid.length, n = grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int mArea = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1){
-                    maxArea = Math.max(maxArea, bfs(grid, i, j));
+                    int area = dfs(i,j,grid);
+                    mArea = Math.max(mArea,area);
                 }
             }
         }
-        return maxArea;
+        return mArea;
     }
-    public int bfs(int[][]grid, int i , int j){
-        int[]dr = {1, -1, 0, 0};
-        int[]dc = {0, 0, 1, -1};
-        grid[i][j]=0;
+    
+    int[] dRow = new int[] {0,0,1,-1};
+    int[] dCol = new int[] {1,-1,0,0};
+
+    public int dfs(int r, int c, int[][]grid){
+        // base case 
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==0) return 0;
+        grid[r][c] = 0;
+        // recursive case
         int area = 1;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        while(q.size()>0){
-            int[]temp = q.poll();
-            for(int k=0;k<4;k++){
-                int nr = temp[0]+dr[k];
-                int nc = temp[1]+dc[k];
-                if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
-                    area++;
-                    q.add(new int[]{nr, nc});
-                    grid[nr][nc] = 0;
-                }
-            }
+        for(int i = 0 ; i < 4; i++){
+            int newRow = r+dRow[i];
+            int newCol = c+dCol[i];
+            area+=dfs(newRow, newCol, grid);
         }
         return area;
-    }
+     }
 }
