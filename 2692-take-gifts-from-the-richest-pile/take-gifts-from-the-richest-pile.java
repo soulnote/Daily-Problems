ae5871c54055800bca7ebb09db64f3a4827db148
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-12-12 14:44:45 +0530

    Time: 7 ms (27.26%) | Memory: 42.6 MB (30.16%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(long num : gifts){
+            pq.add(num);
+        }
+        for(int i=0;i<k;i++){
+            double sq = Math.sqrt(pq.poll());
+
+            long floor = (long)Math.floor(sq);
+            pq.add(floor);
+        }
+        long sum = 0;
+        while(pq.size()>0){
+            sum+=pq.poll();
+        }
+        return sum;
+    }
+}
