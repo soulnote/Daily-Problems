39b4c0228753b3a84170a7c2405b124bc97eaee3
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2024-12-17 01:09:45 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (21.40%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int n = nums.length;
-        int[][] arr = new int[n][2];
-        // PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],
-        // b[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[0] != b[0]) {
-                return Integer.compare(a[0], b[0]);
-            } else {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
-        for (int i = 0; i < n; i++) {
-            pq.add(new int[] { nums[i], i });
+        int[] arr=new int[2];
+        
+        for(int i=0; i<k; i++){
+            arr=getMin(nums);
+            nums[arr[1]]=arr[0]*multiplier;
         }
-
-        while (k-- > 0) {
-            int[] smallest = pq.poll();
-            smallest[0] = smallest[0] * multiplier;
-            pq.add(smallest);
-        }
-        while (!pq.isEmpty()) {
-            int[] e = pq.poll();
-            nums[e[1]] = e[0];
-        }
+        
         return nums;
+    } 
+    
+    int[] getMin(int[] nums){
+        int min=Integer.MAX_VALUE;
+        int idx=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<min){
+                min=nums[i];
+                idx=i;
+            }
+        }
+        
+        return new int[]{min,idx};
     }
+    
 }
