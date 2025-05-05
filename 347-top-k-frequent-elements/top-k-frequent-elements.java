3072ed73efd08ad7cdc654b88617d8fd5310be82
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-05-05 21:12:36 +0530

    Time: 13 ms (76.70%) | Memory: 48.8 MB (52.80%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,23 @@
 class Solution {
-  public int[] topKFrequent(int[] nums, int k) {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for (int a : nums) {
-      map.put(a, map.getOrDefault(a, 0) + 1);
-    }
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int  i=0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        for(int a : map.keySet()){
+            int[] elm = new int[]{a, map.get(a)};
+            pq.offer(elm);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[] ans = new int[k];
+        for(int i=0;i<k;i++){
+            int[] temp = pq.poll();
+            ans[i] = temp[0];
+        }
+        return ans;
 
-    int[][] newArr = new int[map.size()][2];
-    int idx = 0;
-    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-      newArr[idx] = new int[] { entry.getKey(), entry.getValue() };
-      idx++;
     }
-
-    Arrays.sort(newArr, (int[] a, int[] b) -> b[1] - a[1]); // Sort by frequency (descending)
-
-    int[] ans = new int[k];
-    for (int i = 0; i < k; i++) {
-      ans[i] = newArr[i][0];
-    }
-    return ans;
-  }
 }
