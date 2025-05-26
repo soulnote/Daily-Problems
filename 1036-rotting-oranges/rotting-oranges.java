ba82e0340421193d8fc5df19331a155b9f5b44f6
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-05-26 19:25:05 +0530

    Time: 2 ms (68.37%) | Memory: 42.5 MB (62.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,40 +1,48 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int fresh = 0,rotten =0;
+        int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1)fresh++;
-                if(grid[i][j]==2){
-                    q.offer(new int[]{i,j});
-                    rotten++;
-                }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) fresh++;
+                if (grid[i][j] == 2) q.offer(new int[]{i, j});
             }
         }
-        if(rotten==0 && fresh==0) return 0;
-        int time =0;
-        int[]dRow = new int[]{0,0,1,-1};
-        int[]dCol = new int[]{1,-1,0,0};
-        while(!q.isEmpty()){
-            int s = q.size();
-            for(int i = 0; i<s; i++){
-                int[]top = q.poll();
+
+        if (fresh == 0) return 0;
+        if (q.isEmpty()) return -1;
+
+        int time = 0;
+        int[] dRow = {0, 0, 1, -1};
+        int[] dCol = {1, -1, 0, 0};
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean rottedThisMinute = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] top = q.poll();
                 int row = top[0];
                 int col = top[1];
-                for(int k=0;k<4;k++){
+
+                for (int k = 0; k < 4; k++) {
                     int nRow = row + dRow[k];
                     int nCol = col + dCol[k];
-                    if(nRow>=0 && nCol>=0 && nRow<grid.length && nCol<grid[0].length && grid[nRow][nCol]==1){
-                        grid[nRow][nCol]=2;
+
+                    if (nRow >= 0 && nCol >= 0 && nRow < grid.length && nCol < grid[0].length
+                        && grid[nRow][nCol] == 1) {
+                        grid[nRow][nCol] = 2;
                         fresh--;
-                        q.offer(new int[]{nRow,nCol});
+                        q.offer(new int[]{nRow, nCol});
+                        rottedThisMinute = true;
                     }
                 }
             }
-            time++; 
+
+            if (rottedThisMinute) time++; // Only increase time if something got rotted
         }
-        if(fresh!=0)return -1;
-        
-        return time-1;//because while loop runs one extra time when all the oranges are rotten
+
+        return fresh == 0 ? time : -1;
     }
 }
