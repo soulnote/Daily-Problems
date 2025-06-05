e1d391dc466392d89aae4a12a39a3b22d6364113
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-06-05 18:02:37 +0530

    Time: 8 ms (92.08%) | Memory: 45.8 MB (57.50%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,41 +1,29 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=0;i<word.length();i++){ //1
-            
-            char ch = word.charAt(i);
-            if(map.containsKey(ch)){//2
-                map.put(ch,map.get(ch)+1);
-            }//2
-            else{//3
-                map.put(ch,1);
-                // list.add(ch);
-            }//3
+      
+        int[]arr = new int[26];
+        for(int i=0;i<word.length();i++){
+            arr[word.charAt(i)-'a']++;
         }
-        for (Map.Entry<Character,Integer> entry : map.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            list.add(value);
+        Arrays.sort(arr);
+        for(int i=0;i<13;i++){
+            int temp = arr[i];
+            arr[i] = arr[25-i];
+            arr[25-i] = temp;
         }
-        Collections.sort(list, Collections.reverseOrder());
-               int sum=0;
-               for(int i=0;i<list.size();i++){
-                   int ch = list.get(i);
-                   if((i+1)<=8){
-                       sum+= list.get(i)*1*1;
-                   }
-                   else if((i+1)<=16){
-                       sum+= list.get(i)*2;
-                   }
-                   else if((i+1)<=24){
-                       sum+= list.get(i)*3;
-                   }
-                   else sum+=list.get(i)*4;
-               }
-               
-               
-        return sum;
+        int pushes =0;
+        for(int i =0;i<8;i++){
+            pushes+=arr[i];
+        }
+        for(int i=8;i<16;i++){
+            pushes+= arr[i]*2;
+        }
+        for(int i=16;i<24;i++){
+            pushes+= arr[i]*3;
+        }
+        for(int i=24;i<26;i++){
+            pushes+= arr[i]*4;
+        }
+        return pushes;
     }
 }
