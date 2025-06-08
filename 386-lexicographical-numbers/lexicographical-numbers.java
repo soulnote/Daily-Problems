b9c858678f770558d15f95f7bc19e9b2e90de914
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-06-08 14:20:49 +0530

    Time: 5 ms (56.53%) | Memory: 48.7 MB (34.13%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> al = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         int curr = 1;
+        
         for (int i = 1; i <= n; i++) {
-            al.add(curr);
-            if (curr * 10 <= n)
-                curr = curr * 10;
-            else {
-                while (curr % 10 == 9 || curr >= n) {
-                    curr = curr / 10;
+            result.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;  // go deeper
+            } else {
+                while (curr % 10 == 9 || curr + 1 > n) {
+                    curr /= 10;  // backtrack
                 }
-                curr += 1;
+                curr += 1;  // move to next sibling
             }
         }
-        return al;
-
+        
+        return result;
     }
 }
