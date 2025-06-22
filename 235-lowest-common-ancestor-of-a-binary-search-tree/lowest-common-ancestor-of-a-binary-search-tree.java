36492c8d44ab3e444838c55dfca132fc13d6514b
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-06-22 23:47:01 +0530

    Time: 5 ms (100.00%) | Memory: 45.3 MB (38.17%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,27 +9,13 @@
  */
 
 class Solution {
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> listP = new ArrayList<>();
-        ArrayList<TreeNode> listQ = new ArrayList<>();
-        pathTraverse(root,p,listP);
-        pathTraverse(root,q,listQ);
-       
-        int n = Math.min(listP.size(),listQ.size());
-        int idx=0;
-        while(idx<n && listP.get(idx)==listQ.get(idx)){
-            idx++;
-        }
-        return listP.get(idx-1);
-    }
-    public void pathTraverse(TreeNode root, TreeNode p,ArrayList<TreeNode> list){
-        if(root==null)return ;
-        list.add(root);
-        if(root.val==p.val)return;
-        if(root.val<p.val){
-            pathTraverse(root.right,p,list);
-        }
-        else pathTraverse(root.left,p,list);
-        
+        if (p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
     }
 }
