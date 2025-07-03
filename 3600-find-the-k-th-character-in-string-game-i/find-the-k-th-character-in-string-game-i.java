8478bc2bae8c3b5a8900fd6a6abe4dfb2c586f18
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-07-03 05:52:36 +0530

    Time: 3 ms (74.89%) | Memory: 42.6 MB (55.14%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,27 +1,20 @@
 class Solution {
     public char kthCharacter(int k) {
-        int n = 0;
-        while(Math.pow(2,n)<k){
-            n++;
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append('a');
-        while(n>0){
-            StringBuilder s = new StringBuilder();
-            for(int i=0;i<sb.length();i++){
-                char ch = sb.charAt(i);
-                if(ch-'a'==25){
-                    s.append('a');
-                }
-                else{
-                    ch = (char)(ch+1);
-                    s.append(ch);
-                }
+        StringBuilder word = new StringBuilder("a");
+
+        while (word.length() < k) {
+            int len = word.length();
+            StringBuilder next = new StringBuilder();
+            
+            for (int i = 0; i < len; i++) {
+                char currentChar = word.charAt(i);
+                char nextChar = (currentChar == 'z') ? 'a' : (char)(currentChar + 1);
+                next.append(nextChar);
             }
-            sb.append(s.toString());
-            s = null;
-            n--;
+            word.append(next);
+            next = null;
         }
-        return sb.charAt(k-1);
+
+        return word.charAt(k - 1);
     }
 }
