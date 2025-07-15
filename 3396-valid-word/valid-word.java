388456d6a1a020fd661bff09925c28d2b154d2a1
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-07-15 08:18:14 +0530

    Time: 18 ms (9.93%) | Memory: 44.4 MB (5.96%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length()<3)return false;
+        boolean isDigit = true, isVowelPresent = false, isConstantPresent = false;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(!Character.isLetterOrDigit(ch))return false;
+            if(Character.isDigit(ch)){
+                isDigit = true;
+                System.out.println(ch + "is digit");
+            }
+            if(Character.isLetter(ch)){
+                System.out.println(ch + "is letter");
+                if(isVowel(ch))isVowelPresent=true;
+                else isConstantPresent=true;
+            }
+        }
+        return isDigit && isVowelPresent && isConstantPresent;
+    }
+    public boolean isVowel(char ch){
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');set.add('A');
+        set.add('e');set.add('E');
+        set.add('o');set.add('O');
+        set.add('i');set.add('I');
+        set.add('u');set.add('U');
+        if(set.contains(ch))return true;
+        return false;
+    }
+}
