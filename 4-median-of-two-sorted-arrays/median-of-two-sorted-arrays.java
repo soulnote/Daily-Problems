f87c2111725c24aa8188a1977a50f0db07e02fe3
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-07-17 21:54:41 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (98.90%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,34 @@
 class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int totalLength = m + n;
 
-    public double findMedianSortedArrays(int[] a, int[] b) {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int prevElement = 0;
+        int currentElement = 0;
 
-        int n1 = a.length, n2 = b.length;
+        int targetCount = (totalLength / 2) + 1;
 
-        if(n1>n2)
-        return findMedianSortedArrays(b,a);
+        while (count < targetCount) {
+            prevElement = currentElement;
 
-        int n = n1+n2;
-        int left = (n1+n2+1)/2;
-
-        int low =0, high = n1;
-
-        while(low<=high){
-
-            int mid1 = (low + high)/2;
-            int mid2 = left - mid1;
-
-            int l1 = (mid1>0)?a[mid1-1]:Integer.MIN_VALUE;
-            int l2 = (mid2>0)?b[mid2-1]:Integer.MIN_VALUE;
-            int r1 = (mid1<n1)?a[mid1]:Integer.MAX_VALUE;
-            int r2 = (mid2<n2)?b[mid2]:Integer.MAX_VALUE;
-
-            if(l1 <= r2 && l2 <= r1){
-                if(n%2==1)
-                return Math.max(l1,l2);
-                else
-                return ((double)(Math.max(l1,l2) + Math.min(r1,r2)))/2.0;
-            }else if(l1>r2)
-                high = mid1-1;
-            else low = mid1+1;
+            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
+                currentElement = nums1[i];
+                i++;
+            } else {
+                currentElement = nums2[j];
+                j++;
+            }
+            count++;
         }
 
-        return 0;
+        if (totalLength % 2 == 1) {
+            return (double) currentElement;
+        } else {
+            return (double) (prevElement + currentElement) / 2.0;
+        }
     }
 }
