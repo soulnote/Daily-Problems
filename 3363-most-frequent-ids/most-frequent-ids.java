fc6b9d7645cb9825f8330cfd664cd3344e9d6e35
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2025-10-18 17:46:05 +0530

    Time: 71 ms (83.84%) | Memory: 62.2 MB (94.95%) - LeetSync

--- 3363-most-frequent-ids/most-frequent-ids.java
+++ 3363-most-frequent-ids/most-frequent-ids.java
@@ -2,17 +2,14 @@ class Solution {
     public long[] mostFrequentIDs(int[] nums, int[] freq) {
         long[] ans = new long[nums.length];
         HashMap<Integer, Long> idToFreqMap = new HashMap<>();
-        TreeMap<Long, Integer> freqToIdCountMap = new TreeMap<>();
+        PriorityQueue<Pair<Long, Integer>> maxHeap = new PriorityQueue<>(
+                (a, b) -> Long.compare(b.getKey(), a.getKey()));
         for (int i = 0; i < nums.length; ++i) {
-            if (idToFreqMap.getOrDefault(nums[i], 0L) != 0) {
-                freqToIdCountMap.put(idToFreqMap.get(nums[i]), freqToIdCountMap.get(idToFreqMap.get(nums[i])) - 1);
-                if (freqToIdCountMap.get(idToFreqMap.get(nums[i])) == 0)
-                    freqToIdCountMap.remove(idToFreqMap.get(nums[i]));
-            }
             idToFreqMap.put(nums[i], idToFreqMap.getOrDefault(nums[i], 0L) + freq[i]);
-            freqToIdCountMap.put(idToFreqMap.get(nums[i]),
-                    freqToIdCountMap.getOrDefault(idToFreqMap.get(nums[i]), 0) + 1);
-            ans[i] = freqToIdCountMap.lastKey();
+            maxHeap.add(new Pair<>(idToFreqMap.get(nums[i]), nums[i]));
+            while (idToFreqMap.get(maxHeap.peek().getValue()) != maxHeap.peek().getKey())
+                maxHeap.poll();
+            ans[i] = maxHeap.peek().getKey();
         }
         return ans;
     }
