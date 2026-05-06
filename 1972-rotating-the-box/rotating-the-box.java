76c136dad2f641ef43bfc98de1541933befe51cb
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-05-06 06:23:48 +0530

    Time: 7 ms (93.03%) | Memory: 125 MB (13.46%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,15 +1,34 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        int m = box.length, n = box[0].length;
-        char[][] res = new char[n][m];
-        for (int i = 0; i < m; ++i)
-            for (int j = n - 1, k = n - 1; j >= 0; --j) {
-                res[j][m - i - 1] = '.';
-                if (box[i][j] != '.') {
-                    k = box[i][j] == '*' ? j : k;
-                    res[k--][m - i - 1] = box[i][j];
+        int m = box.length;
+        int n = box[0].length;
+
+        // Step 1: Apply gravity to the right
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1; // where next stone should go
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    empty = j - 1; // reset due to obstacle
+                } 
+                else if (box[i][j] == '#') {
+                    // move stone to 'empty' position
+                    box[i][j] = '.';
+                    box[i][empty] = '#';
+                    empty--;
                 }
             }
-        return res;
+        }
+
+        // Step 2: Rotate 90° clockwise
+        char[][] result = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][m - 1 - i] = box[i][j];
+            }
+        }
+
+        return result;
     }
 }
