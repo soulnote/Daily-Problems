e7dcfbcf20101bd165e4fcbe632a5d0d6082b550
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-05-08 12:41:43 +0530

    Time: 493 ms (33.34%) | Memory: 187.9 MB (85.85%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
@@ -77,9 +77,21 @@ class Solution {
     }
 
     // Prime check
-    private boolean isPrime(int x){
-        if (x < 2) return false;
-        for (int i = 2; i * i <= x; i++) if (x % i == 0) return false;
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
