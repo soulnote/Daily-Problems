0edc7c956f03c24a8233f1957932d3293574fe7c
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-05-13 21:24:46 +0530

    Time: 3 ms (98.53%) | Memory: 47.5 MB (6.01%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,31 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][]memo = new int[word1.length()][word2.length()];
-        for(int[]num : memo){
+        int[][] memo = new int[word1.length()][word2.length()];
+        for (int[] num : memo) {
             Arrays.fill(num, -1);
         }
         int ops = solve(word1, word2, 0, 0, memo);
         return ops;
     }
-    public int solve(String word1, String word2, int i, int j, int[][]memo){
-        if(i>=word1.length())return word2.length() - j;
-        if(j>=word2.length())return word1.length() - i;
-        if(memo[i][j]!=-1)return memo[i][j];
-        if(word1.charAt(i)==word2.charAt(j)){
-            return solve(word1, word2, i+1, j+1, memo);
+
+    public int solve(String word1, String word2, int i, int j, int[][] memo) {
+        if (i >= word1.length())
+            return word2.length() - j;
+        if (j >= word2.length())
+            return word1.length() - i;
+        if (memo[i][j] != -1)
+            return memo[i][j];
+        int ans = 0;
+        if (word1.charAt(i) == word2.charAt(j)) {
+            ans = solve(word1, word2, i + 1, j + 1, memo);
+        } else {
+            int insert = solve(word1, word2, i, j + 1, memo);
+            int replace = solve(word1, word2, i + 1, j, memo);
+            int delete = solve(word1, word2, i + 1, j + 1, memo);
+            ans = 1 + Math.min(insert, Math.min(replace, delete));
         }
-        int insert = solve(word1, word2,i, j+1, memo);
-        int replace = solve(word1, word2,i+1, j, memo);
-        int delete = solve(word1, word2, i+1, j+1, memo);
-        int min = 1 + Math.min(insert, Math.min(replace, delete));
-        memo[i][j] = min;
-        return min;
+
+        memo[i][j] = ans;
+        return ans;
     }
 }
