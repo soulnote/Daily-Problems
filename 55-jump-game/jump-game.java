ec78da2faa9f3317ebe0828e5c132eb7e45112ed
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-05-17 10:22:59 +0530

    Time: 320 ms (7.62%) | Memory: 48.3 MB (8.63%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,37 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
+        // Memoization array: 0 = uncomputed, 1 = can reach end, -1 = cannot reach end
+        int[] memo = new int[nums.length];
+        return canJumpFromPosition(0, nums, memo);
+    }
+    
+    private boolean canJumpFromPosition(int position, int[] nums, int[] memo) {
+        // Base case: reached the last index
+        if (position == nums.length - 1) {
+            return true;
+        }
+        
+        // Check if we've already computed this position
+        if (memo[position] != 0) {
+            return memo[position] == 1;
+        }
+        
+        // Get the maximum jump length from current position
+        int maxJump = nums[position];
+        
+        // Try all possible jump lengths from 1 to maxJump
+        // We jump to the furthest positions first for optimization
+        int furthestJump = Math.min(position + maxJump, nums.length - 1);
+        
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums, memo)) {
+                memo[position] = 1;
+                return true;
             }
         }
-
-        return goal == 0;        
+        
+        // If no jump leads to the end
+        memo[position] = -1;
+        return false;
     }
 }
