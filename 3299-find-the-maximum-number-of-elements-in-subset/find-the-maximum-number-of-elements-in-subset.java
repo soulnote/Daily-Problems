e1b190275e8be83bd8fdc8498533d87656cf2025
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-06-27 15:21:39 +0530

    Time: 110 ms (25.58%) | Memory: 79.9 MB (5.81%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -1,22 +1,49 @@
 class Solution {
     public int maximumLength(int[] nums) {
-        Map<Long, Integer> freq = new HashMap<>();
-        for(int num: nums) freq.put((long)num, freq.getOrDefault((long)num, 0) + 1);
-        int res = 1;
-        for(long key: freq.keySet()) {
-            int cur = 0;
-            if(key == 1) {
-                int val = freq.get(key);
-                cur = val % 2 != 0 ? val : val - 1;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            if(map.containsKey(num)){
+                map.get(num).add(i);
             } else {
-                while(freq.getOrDefault(key, 0) >= 2) {
-                    cur += 2;
-                    key = key * key;
-                }
-                cur = (freq.getOrDefault(key, 0) == 1) ? cur + 1 : cur - 1;
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(num, list);
             }
-            res = Math.max(res, cur);
         }
-        return res;
+        
+        int max = 0;
+        
+        if (map.containsKey(1)) {
+            int count1 = map.get(1).size();
+            max = (count1 % 2 == 0) ? count1 - 1 : count1;
+        }
+        
+        for(int i=0; i<nums.length; i++){
+            long num = nums[i];
+            if (num == 1) continue;
+            
+            long curr = num;
+            int count = 0;
+            
+            while(map.containsKey((int)curr) && map.get((int)curr).size() >= 2){
+                count += 2;
+                if (curr > 46340) { 
+                    curr = curr * curr;
+                    break;
+                }
+                curr = curr * curr; 
+            }
+            
+            if(map.containsKey((int)curr)){
+                count += 1;
+            } else {
+                count -= 1;
+            }
+            
+            max = Math.max(max, count);
+        }
+        
+        return max;
     }
 }
