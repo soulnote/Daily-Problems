4184e96518149cecb0a755346bc6726dceeca759
Prateek Singh <71943647+soulnote@users.noreply.github.com>
2026-07-13 03:05:58 +0530

    Time: 48 ms (18.54%) | Memory: 79.5 MB (5.29%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,22 +1,30 @@
 class Solution {
-    public int[] arrayRankTransform(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 1;
-        int[] arr = new int[nums.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = nums[i];
-        }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            if (!map.containsKey(arr[i])) {
-                map.put(arr[i], count);
-                count++;
+    public int[] arrayRankTransform(int[] arr) {
+        int[]ans = new int[arr.length];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            
+            if(map.containsKey(arr[i])){
+                map.get(arr[i]).add(i);
+            }
+            else{
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(arr[i], list);
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            nums[i] = map.get(nums[i]);
+        Arrays.sort(arr);
+        int count = 1;
+        for(int i=0;i<arr.length;i++){
+            int num = arr[i];
+            if(!map.containsKey(num))continue;
+            List<Integer> idxes = map.get(num);
+            map.remove(num);
+            for(int j=0;j<idxes.size();j++){
+                ans[idxes.get(j)] = count;
+            }
+            count++;
         }
-        return nums;
-
+        return ans;
     }
 }
